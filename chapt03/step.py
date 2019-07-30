76b5b43686e41a3a9e2333a0ba5a6d10dc02a288
조석규 <ezaurum@gmail.com>
2019-07-30 12:56:35 +0900

    Add relu

--- chapt03/step.py
+++ chapt03/step.py
@@ -10,11 +10,17 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
 x = np.arange(-5, 5, 0.1)
 
 y = step_function(x)
 y2 = sigmoid(x)
+y3 = relu(x)
 
 plt.plot(x, y)
 plt.plot(x, y2)
+plt.plot(x, y3, ":")
 plt.show()
