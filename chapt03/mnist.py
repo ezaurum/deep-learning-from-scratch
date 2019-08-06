79c7e93a364a24de895a820aab225d99f0481dac
조석규 <ezaurum@gmail.com>
2019-08-06 15:33:16 +0900

    Add minst loader

---
+++ chapt03/mnist.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True)
+
+print(x_train)
