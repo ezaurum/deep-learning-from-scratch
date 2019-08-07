3e5de6096afcb2814ec55f4188f2983ab980811c
조석규 <ezaurum@gmail.com>
2019-08-07 12:31:33 +0900

    Add test log

--- chapt03/mnist.py
+++ chapt03/mnist.py
@@ -6,4 +6,7 @@ from dataset.mnist import load_mnist
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True)
 
-print(x_train)
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
