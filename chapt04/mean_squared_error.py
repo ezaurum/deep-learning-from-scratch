49f06d33c6b47c5aed6d50d7ae9140210ab0e6c0
조석규 <ezaurum@gmail.com>
2019-09-03 12:54:08 +0900

    Add function

--- chapt04/mean_squared_error.py
+++ chapt04/mean_squared_error.py
@@ -1,4 +1,8 @@
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 def mean_squared_error(y, t):
@@ -17,3 +21,29 @@ def cross_entropy_error(y, t):
 
     batch_size = y.shape
     return -np.sum(t * np.log(y[np.arange(batch_size), t])) / batch_size
+
+
+def numerical_diff(f, x):
+    h = 1e-4  # 0.0001
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def function_1(x):
+    return 0.01 * x ** 2 + 0.1 * x
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1]**2
+
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+x1 = np.arange(-10, 10, 0.1)
+x0 = np.arange(-10, 10, 0.1)
+y = np.sum(x1**2)
+ax.plot(x1, x1, y, label='parametric curve')
+ax.legend()
+
+plt.show()
